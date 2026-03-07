07b18d828ee3110509e492a2bac8a1e504871beb
ankittiwari373164 <ankittiwari.373164@gmail.com>
2026-03-07 12:41:09 +0530

    fix sitemap types

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -2,8 +2,17 @@ export const dynamic = 'force-dynamic'
 
 const SITE_URL = process.env.SITE_URL || 'https://thetechbharat.com'
 
+interface SitemapEntry {
+  url: string
+  priority: string
+  changefreq: string
+  lastmod: string
+  image?: string
+  title?: string
+}
+
 export async function GET() {
-  const staticPages = [
+  const staticPages: SitemapEntry[] = [
     { url: '/',                   priority: '1.0', changefreq: 'hourly',  lastmod: '' },
     { url: '/mobile-news',        priority: '0.9', changefreq: 'hourly',  lastmod: '' },
     { url: '/reviews',            priority: '0.9', changefreq: 'daily',   lastmod: '' },
@@ -19,18 +28,18 @@ export async function GET() {
     { url: '/author',             priority: '0.6', changefreq: 'monthly', lastmod: '' },
   ]
 
-  let articleEntries: { url: string; priority: string; changefreq: string; lastmod: string; image?: string; title?: string }[] = []
-  let storyEntries:   { url: string; priority: string; changefreq: string; lastmod: string; image?: string; title?: string }[] = []
+  let articleEntries: SitemapEntry[] = []
+  let storyEntries: SitemapEntry[] = []
 
   try {
     const { getAllArticlesAsync } = await import('@/lib/store')
     const articles = await getAllArticlesAsync()
     articleEntries = articles.map(a => ({
       url:        `/article/${a.slug}`,
-      lastmod:    (a.updatedDate || a.publishDate).split('T')[0],
+      lastmod:    ((a as any).updatedDate || a.publishDate).split('T')[0],
       priority:   '0.8',
       changefreq: 'weekly',
-      image:      a.featuredImage || '',
+      image:      (a as any).featuredImage || (a as any).imageUrl || '',
       title:      a.title,
     }))
   } catch { /* no articles yet */ }
@@ -48,22 +57,25 @@ export async function GET() {
     }))
   } catch { /* no stories yet */ }
 
-  const allEntries = [...staticPages, ...articleEntries, ...storyEntries]
+  const allEntries: SitemapEntry[] = [...staticPages, ...articleEntries, ...storyEntries]
 
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
-        xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
-${allEntries.map(entry => `  <url>
+  const urlTags = allEntries.map(entry => {
+    const imgTag = entry.image
+      ? `    <image:image>\n      <image:loc>${entry.image}</image:loc>\n      ${entry.title ? `<image:title>${entry.title.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;')}</image:title>` : ''}\n    </image:image>`
+      : ''
+    return `  <url>
     <loc>${SITE_URL}${entry.url}</loc>
     ${entry.lastmod ? `<lastmod>${entry.lastmod}</lastmod>` : ''}
     <changefreq>${entry.changefreq}</changefreq>
     <priority>${entry.priority}</priority>
-    ${entry.image ? `<image:image>
-      <image:loc>${entry.image}</image:loc>
-      ${entry.title ? `<image:title>${entry.title.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;')}</image:title>` : ''}
-    </image:image>` : ''}
-  </url>`).join('\n')}
+    ${imgTag}
+  </url>`
+  }).join('\n')
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+${urlTags}
 </urlset>`
 
   return new Response(xml, {
